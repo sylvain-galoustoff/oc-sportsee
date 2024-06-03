6a6bb3008beb282644439248b9b917aa0468fc32
Sylvain-MED <146923279+Sylvain-MED@users.noreply.github.com>
2024-06-03 16:07:17 +0200

    Radar colors updated

--- src/components/common/Radar/Stats.tsx
+++ src/components/common/Radar/Stats.tsx
@@ -1,10 +1,4 @@
-import {
-  PolarGrid,
-  RadarChart,
-  ResponsiveContainer,
-  PolarAngleAxis,
-  Radar,
-} from "recharts";
+import { PolarGrid, RadarChart, ResponsiveContainer, PolarAngleAxis, Radar } from "recharts";
 import style from "./Radar.module.scss";
 import { StatType } from "../../../models/Models";
 
@@ -16,22 +10,10 @@ function Stats({ data }: PropsType) {
   return (
     <div className={style.graph}>
       <ResponsiveContainer>
-        <RadarChart
-          cx="50%"
-          cy="50%"
-          outerRadius="60%"
-          data={data}
-          height={320}
-        >
-          <PolarGrid />
-          <PolarAngleAxis dataKey="subject" />
-          <Radar
-            name="Mike"
-            dataKey="value"
-            stroke="#E60000"
-            fill="#E60000"
-            fillOpacity={0.6}
-          />
+        <RadarChart cx="50%" cy="50%" data={data} height={320}>
+          <PolarGrid stroke="#ffffff" />
+          <PolarAngleAxis dataKey="subject" stroke="#ffffff" />
+          <Radar dataKey="value" stroke="#E60000" fill="#E60000" fillOpacity={0.8} />
         </RadarChart>
       </ResponsiveContainer>
     </div>
