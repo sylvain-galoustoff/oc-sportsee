1d193d2b9ec857378eece44fbd9620d697efc9ba
Sylvain-MED <146923279+Sylvain-MED@users.noreply.github.com>
2024-06-03 13:29:27 +0200

    Session duration hover effect

--- src/components/common/SessionDuration/SessionDuration.tsx
+++ src/components/common/SessionDuration/SessionDuration.tsx
@@ -7,20 +7,39 @@ type PropsType = {
 };
 
 function SessionDuration({ data }: PropsType) {
+  const getActivePoint = () => {
+    const activeDot = document.querySelector(".recharts-active-dot");
+    const parent = document.querySelector("#session-duration");
+    const shader = document.querySelector("#session-shader") as HTMLElement;
+    if (activeDot && parent && shader) {
+      const activeDotRect = activeDot.getBoundingClientRect();
+      const parentRect = parent.getBoundingClientRect();
+
+      // Calculer la coordonnée X par rapport au parent
+      const coordXRelativeToParent = activeDotRect.left - parentRect.left;
+
+      console.log("Coordonnée X par rapport au parent:", coordXRelativeToParent);
+      shader.style.left = coordXRelativeToParent + 3 + "px";
+    }
+  };
+
   return (
     <div className={style.graph} id="session-duration">
+      <div className={style.layer} id="session-shader" />
+      <p className={style.legend}>Durée moyenne des sessions</p>
       <ResponsiveContainer height={320}>
         <LineChart
           data={data}
           margin={{
-            top: 40,
+            top: 100,
             left: 20,
-            bottom: 10,
+            bottom: 20,
             right: 20,
           }}
+          onMouseMove={getActivePoint}
         >
-          {/* <XAxis dataKey="day" stroke="#fff" /> */}
-          <Line type="monotone" dataKey="time" stroke="#fff" dot={true} activeDot={true} />
+          <XAxis dataKey="day" stroke="rgba(255, 255, 255, 0.5)" axisLine={false} tickLine={false} />
+          <Line type="monotone" dataKey="time" stroke="#fff" dot={false} activeDot={true} />
           <Tooltip
             contentStyle={{
               backgroundColor: "white",
